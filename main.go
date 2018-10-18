8a5ae7c970a66b4b0eae4a9297c77ecd788f55fa
Greg Horne <greghorne@hotmail.com>
2018-10-18 21:57:24 +0000

    code update

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo
+
 package main
 
 import (
@@ -11,6 +13,8 @@ func main() {
 	fmt.Println("main...")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/v1/getinfo/{xy}", GetXY.Methods("GET"))
-	log.Fatal(http.ListenAndServer(":8000", router))
+	router.HandleFunc("/v1/getinfo/{xy}", V1GetXY).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+func V1GetXY (w http.ResponseWriter, r *http.Request) {}
