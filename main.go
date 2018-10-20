b83bc75e15e53fee46e8f5499e7af4666386de38
greghorne <greghorne@hotmail.com>
2018-10-20 23:51:27 +0000

    adding pg connect and query code

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
-	"encoding/json"
+	// "encoding/json"
+	_ "github.com/lib/pq"
+	"os"
+	"database/sql"
+	"strconv"
 )
 
 func main() {
@@ -26,23 +30,58 @@ type XYInfo struct {
 }
 
 
+func conn() {
+
+	host     := os.Getenv("GO_HOST")
+	database := os.Getenv("GO_DATABASE")
+	user     := os.Getenv("GO_USER")
+	password := os.Getenv("GO_PASSWORD")
+	port     := os.Getenv("GO_PORT")
+
+	strConnect := "host=" + host + " database=" + database + " user=" + user + " password=" + password + " port=" + port + " sslmode=require"
+	fmt.Println(strConnect)
+	return
+	// db, err := sql.Open("postgres", strConnect)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// return db
+	
+}
+
+// func doQuery(db, lng, lat) {
+// 	row, err := db.Query('select z_tl_2016_us_state($1, $2)', lng, lat)
+// 	return row
+// }
+
+
 func v1GetXY (w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("=====================")
 	params := mux.Vars(r)
-	fmt.Println(params)
-	fmt.Println(params["lng"])
-	fmt.Println(params["lat"])
-	fmt.Println("=====================")
-
-	// create some data
-	var info XYInfo
-	info.Country = "USA"
-	info.Name1	 = "Oklahoma"
-	info.Name2   = "Tulsa"
+	lng := strconv.ParseFloat(params["lng"], 64)
+	lat := strconv.ParseFloat(params["lat"], 64)
+	fmt.Println(lng + ", " + lat)
 	
 	// pass data back to client as JSON
-	json.NewEncoder(w).Encode(info)
+	// json.NewEncoder(w).Encode(info)
+
+	host     := os.Getenv("GO_HOST")
+	database := os.Getenv("GO_DATABASE")
+	user     := os.Getenv("GO_USER")
+	password := os.Getenv("GO_PASSWORD")
+	port     := os.Getenv("GO_PORT")
+
+	strConnect := "host=" + host + " database=" + database + " user=" + user + " password=" + password + " port=" + port + " sslmode=require"
+	fmt.Println(strConnect)
+
+	db, err := sql.Open("postgres", strConnect)
+	fmt.Println(db)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		row, err := db.Query('select z_tl_2016_us_state($1, $2)', lng, lat)
+	}
+	// fmt.Println(conn)
 	
 
 }
